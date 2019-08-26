430c11eadbd5c52f5309553e60d4af0df5460c49
Elizabeth1992Nov5 <elizabethgeorge1992@gmail.com>
2019-08-26 09:43:24 +0530

    angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CartComponent } from './cart/cart.component';
+import { DemoComponent } from './demo/demo.component';
+import { CrudComponent } from './crud/crud.component';
+import { NamedisplayComponent } from './namedisplay/namedisplay.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CartComponent,
+    DemoComponent,
+    CrudComponent,
+    NamedisplayComponent
   ],
   imports: [
     BrowserModule
